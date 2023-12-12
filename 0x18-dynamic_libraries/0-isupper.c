09335310201f9c26782d83cb61eac88949809325
Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>
2023-12-12 03:38:19 +0200

    c

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
+ * _isupper - this checks for uppercase character
+ * @c: is the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
